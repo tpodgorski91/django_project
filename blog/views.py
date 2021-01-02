8cf68425f401d920573e908f0becfe858ba86356
Maszto071 <tpodgorski@mmm.com>
2021-01-02 12:29:44 +0100

    added user registration form

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'JSON',
-        'title': 'post 1',
-        'content': 'this is first post',
-        'date_posted': '19 Nov 2020'
-    },
-    {
-        'author': 'mast071',
-        'title': 'post 2',
-        'content': 'this is second post',
-        'date_posted': '20 Nov 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
