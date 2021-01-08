d225649f0b7f9d7bc36313031a3bb89a33dcedf8
Maszto071 <tpodgorski@mmm.com>
2021-01-08 20:20:10 +0100

    added profile functionality

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    objects = None
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
