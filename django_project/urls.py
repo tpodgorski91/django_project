273645e66de275e6bc089979171d40e92bf86fa7
Your Name <tomposki@gmail.com>
2021-02-23 20:53:57 +0100

    updated password reset tmplt

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    # path('', include('blog.urls')),
     path('password_reset',
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset.html'
