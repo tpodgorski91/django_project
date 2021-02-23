1bd8e9f5a34e95b3eaa4203f6e9d716fa11c8718
Your Name <tomposki@gmail.com>
2021-02-23 20:14:37 +0100

    fixed issue related to password reset path

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,13 +26,28 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('blog.urls')),
+    # path('', include('blog.urls')),
     path('password_reset',
-         auth_views.PasswordResetView.as_view(),
-         name='users/password-reset'),
-    path('password_reset_confirm',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='users/password-reset-confirm')
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+    path('', include('blog.urls')),
 ]
 
 
