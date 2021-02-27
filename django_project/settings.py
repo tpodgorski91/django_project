7afa77e93d710d0db5a54d49f2c94aab14332791
Your Name <tomposki@gmail.com>
2021-02-27 09:59:17 +0100

    Settings Debug Updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['mysuperbblog.herokuapp.com']
 
