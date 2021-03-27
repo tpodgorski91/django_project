966620df75a9287a700d987aa426b0f85cf71588
Your Name <tomposki@gmail.com>
2021-03-27 10:01:25 +0100

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+# comment
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
